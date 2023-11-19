3d428f657a031440c9c84ba87c9de49628fe5c57
Jean Osco <willjean29@gmail.com>
2023-11-19 18:40:53 -0500

    feat: add config envs

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,22 +2,23 @@ import { CheckService } from '../../../modules/checked/domain/useCases/check-ser
 import { FileSystemDatasource } from '../../../modules/logs/infra/datasource/file-system.datasource';
 import { LogRepository } from '../../../modules/logs/infra/repositories/log.repository';
 import { CronService } from '../services/CronService'
-
+import { envs } from '../../../config/plugins/envs.plugin'
 const fileSystemLogRepository = new LogRepository(
   new FileSystemDatasource()
 );
 class Server {
   public static start() {
     console.log("Server started");
-    CronService.createJob('*/2 * * * * *', () => {
-      // console.log('2 second', new Date());
-      const url = 'https://www.gooddgle.com';
-      new CheckService(
-        fileSystemLogRepository,
-        () => console.log(`Success on check service ${url}`),
-        (error) => console.log(error)
-      ).execute(url);
-    });
+    console.log({ envs });
+    // CronService.createJob('*/2 * * * * *', () => {
+    //   // console.log('2 second', new Date());
+    //   const url = 'https://www.gooddgle.com';
+    //   new CheckService(
+    //     fileSystemLogRepository,
+    //     () => console.log(`Success on check service ${url}`),
+    //     (error) => console.log(error)
+    //   ).execute(url);
+    // });
   }
 }
 
