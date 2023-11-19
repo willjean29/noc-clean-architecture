5c9297ad7c0be4c8e1c22165df5a4b22afdb6898
Jean Osco <willjean29@gmail.com>
2023-11-19 01:53:19 -0500

    feat: add implementation log repository

--- src/modules/checked/domain/useCases/check-service.usecase.ts
+++ src/modules/checked/domain/useCases/check-service.usecase.ts
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import { LogRepository } from '../../../logs/domain/repositories/log.repository';
+import { LogEntity, LogSeverityLevel } from '../../../logs/domain/entities/log.entity';
+
 
 interface CheckServiceUseCase {
   execute: (url: string) => Promise<boolean>;
@@ -8,6 +11,7 @@ type ErrorCallback = (error: string) => void;
 
 export class CheckService implements CheckServiceUseCase {
   constructor(
+    private readonly logRepository: LogRepository,
     private successCallback: SuccessCallback,
     private errorCallback: ErrorCallback
   ) { }
@@ -15,10 +19,14 @@ export class CheckService implements CheckServiceUseCase {
   async execute(url: string): Promise<boolean> {
     try {
       await axios.get(url);
+      const log = new LogEntity(`Service ${url} is working`, LogSeverityLevel.Low);
+      this.logRepository.saveLog(log);
       this.successCallback();
       return true
     } catch (error) {
       const errorMessage = `Error on check service ${url}`
+      const log = new LogEntity(errorMessage, LogSeverityLevel.High);
+      this.logRepository.saveLog(log);
       this.errorCallback(errorMessage);
       return false;
     }
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,12 +1,19 @@
 import { CheckService } from '../../../modules/checked/domain/useCases/check-service.usecase';
+import { FileSystemDatasource } from '../../../modules/logs/infra/datasource/file-system.datasource';
+import { LogRepository } from '../../../modules/logs/infra/repositories/log.repository';
 import { CronService } from '../services/CronService'
+
+const fileSystemLogRepository = new LogRepository(
+  new FileSystemDatasource()
+);
 class Server {
   public static start() {
     console.log("Server started");
     CronService.createJob('*/2 * * * * *', () => {
       // console.log('2 second', new Date());
-      const url = 'https://www.google.com';
+      const url = 'https://www.gooddgle.com';
       new CheckService(
+        fileSystemLogRepository,
         () => console.log(`Success on check service ${url}`),
         (error) => console.log(error)
       ).execute(url);
