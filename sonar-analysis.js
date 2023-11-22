845ad1c0242427fc0a047d6226cba803645d0991
Jean Osco <willjean29@gmail.com>
2023-11-22 15:26:54 -0500

    chore: add sonar qube report

---
+++ sonar-analysis.js
@@ -0,0 +1,18 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+console.log({ env: process.env })
+sonarqubeScanner(
+  {
+    serverUrl: 'http://localhost:9000',
+    options: {
+      'sonar.sources': './src', // Ruta a tus archivos fuente TypeScript
+      'sonar.inclusions': '**/*.ts', // Patrón de inclusión para archivos TypeScript
+      'sonar.ts.tscconfigpath': path.resolve('./tsconfig.json'), // Ruta al archivo tsconfig.json
+      'sonar.typescript.lcov.reportPaths': 'coverage/lcov-report/*.lcov', // Si estás generando informes de cobertura
+      'sonar.token': `${process.env.SONAR_TOKEN}` || '', // Reemplaza 'TU_TOKEN_AQUI' con tu token generado
+    },
+  },
+  () => process.exit()
+);
